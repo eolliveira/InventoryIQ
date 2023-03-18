e10080b0b82c6a6502c2d5820b9009c782dc603a
Erick Oliveira <erick.oliveira9607@gmail.com>
2023-03-17 22:05:17 -0300

    customizando sidebar

--- src/components/MenuNavBar/MenuSidebar.tsx
+++ src/components/MenuNavBar/MenuSidebar.tsx
@@ -8,9 +8,11 @@ import {
 } from 'react-pro-sidebar';
 import styled from 'styled-components';
 import AddTaskIcon from '@mui/icons-material/AddTask';
-import { Button } from '@mui/material';
+import { Button, collapseClasses, Typography } from '@mui/material';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import MenuIcon from '@mui/icons-material/Menu';
-import { useState } from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 
 export default function MenuSidebar() {
   const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } =
@@ -27,9 +29,21 @@ export default function MenuSidebar() {
       >
         <Menu>
           <ButtonColapseContainer isColapsed={collapsed}>
+            {!collapsed ? <LinkedInIcon fontSize="large" /> : ''}
+            {!collapsed ? (
+              <Typography variant="h6">LinkedInIcon</Typography>
+            ) : (
+              ''
+            )}
             <Button
               style={{ display: 'flex' }}
-              startIcon={<MenuIcon />}
+              startIcon={
+                collapsed ? (
+                  <MenuIcon color="action" />
+                ) : (
+                  <ArrowBackIosNewIcon color="action" />
+                )
+              }
               onClick={() => collapseSidebar()}
             ></Button>
           </ButtonColapseContainer>
@@ -50,12 +64,14 @@ const SidebarContainer = styled.div`
   height: 100vh;
 `;
 
-
 interface ComponentProps {
-  isColapsed: boolean
+  isColapsed: boolean;
 }
 
 const ButtonColapseContainer = styled.div<ComponentProps>`
-  background-color: ${props => props.isColapsed ? 'red' : 'blue'};
+  background-color: ${(props) => (props.isColapsed ? '#999' : '#999')};
+  justify-content: ${(props) => (props.isColapsed ? 'flex-end' : 'space-between')};
+  height: 50px;
   display: flex;
- `
+  align-items: center;
+`;
